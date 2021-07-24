0b30fd339e00d965c3378ea67a721be9c8e2ee76
Bryan Chen <xlchen1291@gmail.com>
2021-07-24 22:32:04 +1200

    register types

--- packages/page-kitties/src/index.tsx
+++ packages/page-kitties/src/index.tsx
@@ -5,6 +5,23 @@ import type { AppProps as Props } from '@polkadot/react-components/types';
 
 import React from 'react';
 
+import registry from '@polkadot/react-api/typeRegistry';
+
+registry.register({
+  ClassId: 'u32',
+  ClassInfoOf: 'ClassId',
+  Kitty: '[u8; 16]',
+  KittyIndex: 'u32',
+  KittyIndexOf: 'KittyIndex',
+  TokenId: 'u32',
+  TokenInfo: {
+    metadata: 'Vec<u8>',
+    owner: 'AccountId',
+    data: 'Kitty' // eslint-disable-line
+  },
+  TokenInfoOf: 'TokenInfo'
+});
+
 function KittiesApp (): React.ReactElement<Props> {
   return (
     <main>
