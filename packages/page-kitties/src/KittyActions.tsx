10a2e3658202f17ba0a4c769633052c34312484c
Bryan Chen <xlchen1291@gmail.com>
2021-07-25 15:47:57 +1200

    Style improvement

--- packages/page-kitties/src/KittyActions.tsx
+++ packages/page-kitties/src/KittyActions.tsx
@@ -3,9 +3,17 @@
 
 import BN from 'bn.js';
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import { Button, InputAddress, InputBalance, InputNumber, TxButton } from '@polkadot/react-components';
 import { useApi } from '@polkadot/react-hooks';
+
+const ActionWrapper = styled.details`
+   margin-top: 5px;
+   h2 {
+     display: inline-block;
+   }
+ `;
 
 type Props = {
   accountId: string | null;
@@ -23,82 +31,98 @@ const KittyActions: React.FC<Props> = ({ accountId }: Props) => {
   return (
     <section>
       <h1>Kitty Actions</h1>
-      <h2>Create Kitty</h2>
-      <div className='ui--row'>
-        <div className='large'>
-          <Button.Group>
-            <TxButton
-              accountId={accountId}
-              label='Create New Kitty'
-              params={[]}
-              tx={api.tx.kitties.create}
-            />
-          </Button.Group>
+      <ActionWrapper>
+        <summary>
+          <h2>Create Kitty</h2>
+        </summary>
+        <div className='ui--row'>
+          <div className='large'>
+            <Button.Group>
+              <TxButton
+                accountId={accountId}
+                label='Create New Kitty'
+                params={[]}
+                tx={api.tx.kitties.create}
+              />
+            </Button.Group>
+          </div>
         </div>
-      </div>
-      <h2>Breed Kitty</h2>
-      <div className='ui--row'>
-        <div className='large'>
-          <InputNumber
-            label='First Parent Kitty ID'
-            onChange={setParent1}
-          />
-          <InputNumber
-            label='Second Parent Kitty ID'
-            onChange={setParent2}
-          />
-          <Button.Group>
-            <TxButton
-              accountId={accountId}
-              label='Breed Kitty'
-              params={[parent1, parent2]}
-              tx={api.tx.kitties.breed}
+      </ActionWrapper>
+      <ActionWrapper>
+        <summary>
+          <h2>Breed Kitty</h2>
+        </summary>
+        <div className='ui--row'>
+          <div className='large'>
+            <InputNumber
+              label='First Parent Kitty ID'
+              onChange={setParent1}
             />
-          </Button.Group>
-        </div>
-      </div>
-      <h2>Transfer Kitty</h2>
-      <div className='ui--row'>
-        <div className='large'>
-          <InputAddress
-            label='recipient address'
-            onChange={setRecipientId}
-          />
-          <InputNumber
-            label='Kitty ID to send'
-            onChange={setTransferKittyId}
-          />
-          <Button.Group>
-            <TxButton
-              accountId={accountId}
-              label='Transfer Kitty'
-              params={[recipientId, transferKittyId]}
-              tx={api.tx.kitties.transfer}
+            <InputNumber
+              label='Second Parent Kitty ID'
+              onChange={setParent2}
             />
-          </Button.Group>
+            <Button.Group>
+              <TxButton
+                accountId={accountId}
+                label='Breed Kitty'
+                params={[parent1, parent2]}
+                tx={api.tx.kitties.breed}
+              />
+            </Button.Group>
+          </div>
         </div>
-      </div>
-      <h2>Sell Kitty</h2>
-      <div className='ui--row'>
-        <div className='large'>
-          <InputNumber
-            label='Kitty ID to sell'
-            onChange={setSellKittyId}
-          />
-          <InputBalance
-            label='Kitty Price to sell'
-            onChange={setSellKittyPrice}
-          />
-          <Button.Group>
-            <TxButton
-              accountId={accountId}
-              label='Set Kitty Price'
-              params={[sellKittyId, sellKittyPrice]}
-              tx={api.tx.kitties.setPrice}
+      </ActionWrapper>
+      <ActionWrapper>
+        <summary>
+          <h2>Transfer Kitty</h2>
+        </summary>
+        <div className='ui--row'>
+          <div className='large'>
+            <InputAddress
+              label='recipient address'
+              onChange={setRecipientId}
             />
-          </Button.Group>
+            <InputNumber
+              label='Kitty ID to send'
+              onChange={setTransferKittyId}
+            />
+            <Button.Group>
+              <TxButton
+                accountId={accountId}
+                label='Transfer Kitty'
+                params={[recipientId, transferKittyId]}
+                tx={api.tx.kitties.transfer}
+              />
+            </Button.Group>
+          </div>
         </div>
-      </div>
+      </ActionWrapper>
+      <ActionWrapper>
+        <summary>
+          <h2>Sell Kitty</h2>
+        </summary>
+        <div className='ui--row'>
+          <div className='large'>
+            <InputNumber
+              label='Kitty ID to sell'
+              onChange={setSellKittyId}
+            />
+            <InputBalance
+              label='Kitty Price to sell'
+              onChange={setSellKittyPrice}
+            />
+            <Button.Group>
+              <TxButton
+                accountId={accountId}
+                label='Set Kitty Price'
+                params={[sellKittyId, sellKittyPrice]}
+                tx={api.tx.kitties.setPrice}
+              />
+            </Button.Group>
+          </div>
+        </div>
+      </ActionWrapper>
     </section>
   );
 };
