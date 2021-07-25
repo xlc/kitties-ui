382be1b4261aa1bdd3ad0d443c1710e27f030b70
Bryan Chen <xlchen1291@gmail.com>
2021-07-25 15:13:47 +1200

    Able to breed kitty

--- packages/page-kitties/src/KittyActions.tsx
+++ packages/page-kitties/src/KittyActions.tsx
@@ -1,9 +1,10 @@
 // Copyright 2017-2020 @polkadot/app-js authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import BN from 'bn.js';
+import React, { useState } from 'react';
 
-import { Button, TxButton } from '@polkadot/react-components';
+import { Button, InputNumber, TxButton } from '@polkadot/react-components';
 import { useApi } from '@polkadot/react-hooks';
 
 type Props = {
@@ -12,6 +13,8 @@ type Props = {
 
 const KittyActions: React.FC<Props> = ({ accountId }: Props) => {
   const { api } = useApi();
+  const [parent1, setParent1] = useState<BN | undefined>(undefined);
+  const [parent2, setParent2] = useState<BN | undefined>(undefined);
 
   return (
     <section>
@@ -24,6 +27,26 @@ const KittyActions: React.FC<Props> = ({ accountId }: Props) => {
               label='Create New Kitty'
               params={[]}
               tx={api.tx.kitties.create}
+            />
+          </Button.Group>
+        </div>
+      </div>
+      <div className='ui--row'>
+        <div className='large'>
+          <InputNumber
+            label='First Parent Kitty ID'
+            onChange={setParent1}
+          />
+          <InputNumber
+            label='Second Parent Kitty ID'
+            onChange={setParent2}
+          />
+          <Button.Group>
+            <TxButton
+              accountId={accountId}
+              label='Breed Kitty'
+              params={[parent1, parent2]}
+              tx={api.tx.kitties.breed}
             />
           </Button.Group>
         </div>
