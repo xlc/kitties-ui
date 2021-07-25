7118be572bf95914be7d33ee60362d9d0605522c
Bryan Chen <xlchen1291@gmail.com>
2021-07-25 12:11:46 +1200

    add typing

--- packages/page-kitties/src/types.ts
+++ packages/page-kitties/src/types.ts
@@ -1,6 +1,17 @@
 // Copyright 2017-2021 @polkadot/app-staking authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { Struct, u8, U8aFixed, u32, Vec } from '@polkadot/types';
+import { AccountId } from '@polkadot/types/interfaces';
+
+export type Kitty = U8aFixed
+export type KittyIndex = u32
+export interface TokenInfo extends Struct {
+  metadata: Vec<u8>;
+  owner: AccountId;
+  data: Kitty;
+}
+
 export interface ComponentProps {
   className?: string;
 }
